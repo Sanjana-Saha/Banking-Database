f3601cae449e6f0830ccebcb2d20c6731a4a7885
Rishabh Reddy Mathireddy <rishabhreddy.mathireddy@lntinfotech.com>
2020-09-25 19:45:10 +0530

    updated tables

--- Table.sql
+++ Table.sql
@@ -2,22 +2,16 @@ create database Bank
 go
 use Bank
 
-create table CreateSavingsAccount(ReferenceId int Identity(101,1) Primary key clustered,
- UserId AS 'LTI' + RIGHT('00000' + CAST(ReferenceId AS VARCHAR(5)), 5) PERSISTED,
+create table CreateSavingsAccount(ReferenceId int Identity(101,1) unique,
+ UserId AS 'LTI' + RIGHT('00000' + CAST(ReferenceId AS VARCHAR(5)), 5) PERSISTED primary key not null,
 Title varchar(5) not null,FirstName varchar(20) Not Null,MiddleName varchar(20),LastName varchar(20) Not Null
 ,FatherName varchar(25) Not null,MobileNumber Bigint Not Null,EmailId varchar(30),AadharNumber bigint not null,
 DateOfBirth date not null,ResAddress1 varchar(100) not null,ResAddress2 varchar(100) not null,
 ResLandmark varchar(100),ResCity varchar(20) not null,ResState varchar(30) not null,ResPincode bigint not null,
 PerAddress1 varchar(100) not null,PerAddress2 varchar(100) not null,
 PerLandmark varchar(100),PerCity varchar(20) not null,PerState varchar(30) not null,PerPincode bigint not null,
-OccupationType varchar(20) not null,SourceIncome varchar(20) not null,GrossIncome bigint not null,
-DebitcardOpted bit default 0,,AccountStatus bit default 0,AccountRejected bit default 0)
-
-alter table CreatesavingsAccount
-add constraint uniqueid unique(UserId)
-
-select * from CreateSavingsAccount 
-
+OccupationType varchar(20) not null,SourceIncome varchar(20) not null,GrossIncome Decimal(20) not null,
+DebitcardOpted bit default 0,AccountStatus bit default 0,AccountRejected bit default 0)
 
 create table AdminLogin (AdminId varchar(20) primary key,AdminPassword varchar(20) not null)
 
